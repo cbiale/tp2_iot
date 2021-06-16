bf95baec5444178c3d9fffed94de77b8f893d551
cbiale <claudio_biale@yahoo.com.ar>
2021-06-16 19:43:14 -0300

    cambios finales

--- src/lista.js
+++ src/lista.js
@@ -1,67 +1,67 @@
 module.exports = class Lista {
-    // atributos
-    #elementos;
+  // atributos
+  #elementos
 
-    // constructor
-    constructor() {
-        this.#elementos = [];
+  // constructor
+  constructor() {
+    this.#elementos = []
+  }
+
+  // Devuelve la cantidad de elementos de la lista
+  count() {
+    return this.#elementos.length
+  }
+
+  // Devuelve el valor asociado a la clave pasada como argumento
+  find(clave) {
+    for (let i in this.#elementos) {
+      if (this.#elementos[i].clave == clave) {
+        return this.#elementos[i].valor
+      }
     }
+    return NaN
+  }
 
-    // Devuelve la cantidad de elementos de la lista
-    count() {
-        return this.#elementos.length;
-    }
-
-    // Devuelve el valor asociado a la clave pasada como argumento
-    find(clave) {
-        for (let i in this.#elementos) {
-            if (this.#elementos[i].clave == clave) {
-                return this.#elementos[i].valor
-            }
+  // Agrega un par clave / valor (funciona como un mapa o diccionario)
+  // controla la clave sea de tipo String
+  // devuelve verdadero si se agrega o actualiza
+  // devuelve falso si no es de tipo String
+  add(clave, valor) {
+    if (typeof clave == 'string') {
+      for (let i in this.#elementos) {
+        if (this.#elementos[i].clave == clave) {
+          this.#elementos[i].valor = valor
+          return true
         }
-        return NaN;
+      }
+      this.#elementos.push({
+        clave,
+        valor,
+      })
+      return true
+    } else {
+      return false
     }
+  }
 
-    // Agrega un par clave / valor (funciona como un mapa o diccionario)
-    // controla la clave sea de tipo String
-    // devuelve verdadero si se agrega o actualiza
-    // devuelve falso si no es de tipo String
-    add(clave, valor) {
-        if (typeof clave == 'string') {
-            for (let i in this.#elementos) {
-                if (this.#elementos[i].clave == clave) {
-                    this.#elementos[i].valor = valor
-                    return true
-                }
-            }
-            this.#elementos.push({
-                clave,
-                valor
-            });
-        } else {
-            return false;
-        }
+  // Elimina un elemento de acuerdo a laa clave pasada como argumento
+  // retorna verdadero si se elimina un elemento
+  // retorna falso si no se elimina un elemento
+  delete(clave) {
+    for (let i in this.#elementos) {
+      if (this.#elementos[i].clave == clave) {
+        this.#elementos.splice(i)
+        return true
+      }
     }
+    return false
+  }
 
-    // Elimina un elemento de acuerdo a laa clave pasada como argumento
-    // retorna verdadero si se elimina un elemento
-    // retorna falso si no se elimina un elemento
-    delete(clave) {
-        for (let i in this.#elementos) {
-            if (this.#elementos[i].clave == clave) {
-                this.#elementos.splice(i)
-                return true
-            }
-        }
-        return false;
-    }
-
-    // Devuelve una lista ordenada de manera ascendente por clave
-    getListOrdered() {
-        let ordenada = []
-        for (let i in this.#elementos)
-            ordenada.push(this.#elementos[i].clave);
-        ordenada.sort()
-        return ordenada
-    }
+  // Devuelve una lista ordenada de manera ascendente por clave
+  getListOrdered() {
+    let ordenada = []
+    for (let i in this.#elementos) ordenada.push(this.#elementos[i].clave)
+    ordenada.sort()
+    return ordenada
+  }
 }
--- test/test_lista.js
+++ test/test_lista.js
@@ -19,32 +19,35 @@
  * Cuando se agrega un elemento al final la lista de claves esta ordenada.
  */
 
-const assert = require("chai").assert;
-const Lista = require("../src/lista.js");
+const assert = require('chai').assert
+const Lista = require('../src/lista.js')
 
 // * En una lista vacía hay cero elementos.
 // * En una lista vacía no se encuentra ninguna clave.
 // * En una lista vacía la lista de claves devuelve [].
 // * En una lista vacía al eliminar un elemento devuelve false.
-describe("En una lista vacía", function () {
-    var lista = new Lista();
-    it("hay cero elementos", function () {
-        assert.equal(lista.count(), 0);
-    });
+describe('En una lista vacía', function () {
+    var lista = new Lista()
+    it('hay cero elementos', function () {
+        assert.equal(lista.count(), 0)
+    })
 
-    it("no se encuentra ninguna clave", function () {
-        assert.isNaN(lista.find("clave"));
-    });
+    it('no se encuentra ninguna clave', function () {
+        assert.isNaN(lista.find('clave'))
+    })
 
-    it("al recuperar la lista ordenada de claves se obtiene []", function () {
-        assert.deepEqual(lista.getListOrdered(), []);
-    });
+    it('al recuperar la lista ordenada de claves se obtiene []', function () {
+        assert.deepEqual(lista.getListOrdered(), [])
+    })
 
-    it("al eliminar un elemento devuelve false", function () {
-        assert.equal(lista.delete("clave"), false);
-    });
-});
+    it('al eliminar un elemento devuelve false', function () {
+        assert.equal(lista.delete('clave'), false)
+    })
+})
 
+// * Cuando se agrega un elemento a una lista vacía si la clave no es de tipo String devuelve false
+// * Cuando se agrega un elemento a una lista vacía si la clave no es de tipo String, entonces hay 0 elementos
+// * Cuando se agrega un elemento a una lista vacía si la clave es de tipo String devuelve true
 // * Cuando se agrega un elemento a una lista vacía hay un elemento.
 // * Cuando se agrega un elemento a una lista vacía se puede recuperar el valor a partir de la clave.
 // * Cuando se agrega un elemento a una lista vacía si la clave es inexistente no se encuentra ninguna clave.
@@ -52,74 +55,83 @@ describe("En una lista vacía", function () {
 // * Cuando se agrega un elemento a una lista vacía y se elimina se obtiene true.
 // * Cuando se agrega un elemento a una lista vacía y se intenta eliminar nuevamente se obtiene false.
 // * Cuando se agrega un elemento a una lista vacía ale eliminarse la cantidad de elementos es 0.
-describe("Cuando se agrega un elemento a una lista vacía", function () {
-    var lista = new Lista();
-    lista.add("clave", "valor");
+describe('Cuando se agrega un elemento a una lista vacía', function () {
+    var lista = new Lista()
 
-    it("hay un elemento", function () {
-        assert.equal(lista.count(), 1);
-    });
+    it('si la clave no es de tipo String devuelve false', function () {
+        assert.equal(lista.add(1, 'valor'), false)
+    })
 
-    it("se puede recuperar el valor a partir de la clave", function () {
-        assert.equal(lista.find("clave"), "valor");
-    });
+    it('si la clave no es de tipo String, entonces hay 0 elementos', function () {
+        assert.equal(lista.count(), 0)
+    })
 
-    it("si la clave es inexistente no se encuentra ninguna clave", function () {
-        assert.isNaN(lista.find("otra-clave"));
-    });
+    it('si la clave es de tipo String devuelve true', function () {
+        assert.equal(lista.add('clave', 'valor'), true)
+    })
 
-    it("la lista de claves esta ordenada", function () {
-        assert.deepEqual(lista.getListOrdered(), ["clave"]);
-    });
+    it('hay un elemento', function () {
+        assert.equal(lista.count(), 1)
+    })
 
-    it("y se elimina se obtiene true", function () {
-        assert.equal(lista.delete("clave"), true);
-    });
+    it('se puede recuperar el valor a partir de la clave', function () {
+        assert.equal(lista.find('clave'), 'valor')
+    })
 
-    it("y se intenta eliminar nuevamente se obtiene false", function () {
-        assert.equal(lista.delete("clave"), false);
-    });
+    it('si la clave es inexistente no se encuentra ninguna clave', function () {
+        assert.isNaN(lista.find('otra-clave'))
+    })
 
-    it("al eliminarse la cantidad de elementos es 0", function () {
-        assert.equal(lista.count(), 0);
-    });
+    it('la lista de claves esta ordenada', function () {
+        assert.deepEqual(lista.getListOrdered(), ['clave'])
+    })
 
-});
+    it('y se elimina se obtiene true', function () {
+        assert.equal(lista.delete('clave'), true)
+    })
+
+    it('y se intenta eliminar nuevamente se obtiene false', function () {
+        assert.equal(lista.delete('clave'), false)
+    })
+
+    it('al eliminarse la cantidad de elementos es 0', function () {
+        assert.equal(lista.count(), 0)
+    })
+})
 
 // * Cuando se agrega una clave que ya está en la lista se actualiza el valor correspondiente.
 // * Cuando se agrega una clave que ya está en la lista se obtiene el valor asociado a la clave.
 // * Cuando se agrega una clave que ya está en la lista y se elimina se obtiene true.
-describe("Cuando se agrega una clave que está en la lista", function () {
-    var lista = new Lista();
-    lista.add("clave", "valor");
+describe('Cuando se agrega una clave que está en la lista', function () {
+    var lista = new Lista()
+    lista.add('clave', 'valor')
 
-    it("se actualiza el valor correspondiente", function () {
-        assert.equal(lista.add("clave", "nuevo"), true);
-    });
+    it('se actualiza el valor correspondiente', function () {
+        assert.equal(lista.add('clave', 'nuevo'), true)
+    })
 
-    it("se obtiene el nuevo valor asociado a la clave", function () {
-        assert.equal(lista.find("clave"), "nuevo");
-    });
+    it('se obtiene el nuevo valor asociado a la clave', function () {
+        assert.equal(lista.find('clave'), 'nuevo')
+    })
 
-    it("y se elimina se obtiene true", function () {
-        assert.equal(lista.delete("clave"), true);
-    });
-});
+    it('y se elimina se obtiene true', function () {
+        assert.equal(lista.delete('clave'), true)
+    })
+})
 
+describe('Cuando se recupera la lista de claves y hay 5 elementos no ordenados', function () {
+    var lista = new Lista()
+    lista.add('A', 'valorA')
+    lista.add('Z', 'valorZ')
+    lista.add('S', 'valorS')
+    lista.add('B', 'valorB')
+    lista.add('C', 'valorC')
 
-describe("Cuando se recupera la lista de claves y hay 5 elementos no ordenados", function () {
-    var lista = new Lista();
-    lista.add("A", "valorA");
-    lista.add("Z", "valorZ");
-    lista.add("S", "valorS");
-    lista.add("B", "valorB");
-    lista.add("C", "valorC");
+    it('La cantidad de elementos es 5', function () {
+        assert.equal(lista.count(), 5)
+    })
 
-    it("La cantidad de elementos es 5", function () {
-        assert.equal(lista.count(), 5);
-    });
-
-    it("se obtiene una lista de claves ordenadas", function () {
-        assert.deepEqual(lista.getListOrdered(), ["A", "B", "C", "S", "Z"]);
-    });
-});
+    it('se obtiene una lista de claves ordenadas', function () {
+        assert.deepEqual(lista.getListOrdered(), ['A', 'B', 'C', 'S', 'Z'])
+    })
+})
