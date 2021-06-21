a8171a036146b06c9a19bcbd0d21ec4bb5649f17
cbiale <claudio_biale@yahoo.com.ar>
2021-06-21 15:31:13 -0300

    metodo buscarPosicionElemento()

--- src/lista.js
+++ src/lista.js
@@ -1,67 +1,74 @@
 module.exports = class Lista {
-  // atributos
-  #elementos
+    // atributos
+    #elementos
 
-  // constructor
-  constructor() {
-    this.#elementos = []
-  }
-
-  // Devuelve la cantidad de elementos de la lista
-  count() {
-    return this.#elementos.length
-  }
-
-  // Devuelve el valor asociado a la clave pasada como argumento
-  find(clave) {
-    for (let i in this.#elementos) {
-      if (this.#elementos[i].clave == clave) {
-        return this.#elementos[i].valor
-      }
+    // constructor
+    constructor() {
+        this.#elementos = []
     }
-    return NaN
-  }
 
-  // Agrega un par clave / valor (funciona como un mapa o diccionario)
-  // controla la clave sea de tipo String
-  // devuelve verdadero si se agrega o actualiza
-  // devuelve falso si no es de tipo String
-  add(clave, valor) {
-    if (typeof clave == 'string') {
-      for (let i in this.#elementos) {
-        if (this.#elementos[i].clave == clave) {
-          this.#elementos[i].valor = valor
-          return true
+    // buscar elemento
+    // si existe devuelve posición, sino existe devuelve -1
+    #buscarPosicionElemento(clave) {
+        for (let i in this.#elementos) {
+            if (this.#elementos[i].clave == clave) {
+                return i
+            }
         }
-      }
-      this.#elementos.push({
-        clave,
-        valor,
-      })
-      return true
-    } else {
-      return false
+        return -1;
     }
-  }
 
-  // Elimina un elemento de acuerdo a laa clave pasada como argumento
-  // retorna verdadero si se elimina un elemento
-  // retorna falso si no se elimina un elemento
-  delete(clave) {
-    for (let i in this.#elementos) {
-      if (this.#elementos[i].clave == clave) {
-        this.#elementos.splice(i)
-        return true
-      }
+    // Devuelve la cantidad de elementos de la lista
+    count() {
+        return this.#elementos.length
     }
-    return false
-  }
 
-  // Devuelve una lista ordenada de manera ascendente por clave
-  getListOrdered() {
-    let ordenada = []
-    for (let i in this.#elementos) ordenada.push(this.#elementos[i].clave)
-    ordenada.sort()
-    return ordenada
-  }
+    // Devuelve el valor asociado a la clave pasada como argumento
+    find(clave) {
+        let posicion = this.#buscarPosicionElemento(clave);
+        if (posicion !== -1) {
+            return this.#elementos[posicion].valor
+        }
+        return NaN
+    }
+
+    // Agrega un par clave / valor (funciona como un mapa o diccionario)
+    // controla la clave sea de tipo String
+    // devuelve verdadero si se agrega o actualiza
+    // devuelve falso si no es de tipo String
+    add(clave, valor) {
+        if (typeof clave == 'string') {
+            let posicion = this.#buscarPosicionElemento(clave)
+            if (posicion !== -1) {
+                this.#elementos[posicion].valor = valor
+            } else {
+                this.#elementos.push({
+                    clave,
+                    valor,
+                })
+            }
+            return true
+        }
+        return false
+    }
+
+    // Elimina un elemento de acuerdo a laa clave pasada como argumento
+    // retorna verdadero si se elimina un elemento
+    // retorna falso si no se elimina un elemento
+    delete(clave) {
+        let posicion = this.#buscarPosicionElemento(clave)
+        if (posicion !== -1) {
+            this.#elementos.splice(posicion)
+            return true
+        }
+        return false
+    }
+
+    // Devuelve una lista ordenada de manera ascendente por clave
+    getListOrdered() {
+        let ordenada = []
+        for (let i in this.#elementos) ordenada.push(this.#elementos[i].clave)
+        ordenada.sort()
+        return ordenada
+    }
 }
